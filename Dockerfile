14b63061725621adf34f129c20e3ff8076bab537
Mitch Webster <Mitchell.Webster@microsoft.com>
2019-09-11 14:23:28 -0700

    try again 4

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 # Container image that runs your code
 #FROM alpine:3.10
-FROM mcr.microsoft.com/oryx/build:slim
+#FROM mcr.microsoft.com/oryx/build:slim
+FROM ahmelsayed/oryx:node-build
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,9 @@
 echo "Cloning repository"
 curl "https://github.com/miwebst/ReactSite/archive/a2226e70d6ce57295d336431ab41a6c91b48f00c.zip" -L -o source.zip
 echo "Cloned repository"
-echo $(ls)
 unzip -qq source.zip
 echo "Successfully unzipped repository"
-echo $(ls)
 cd ReactSite-a2226e70d6ce57295d336431ab41a6c91b48f00c
-echo $(ls)
 echo "Building app folder"
 oryx build app -o ../staticsitesoutput/app.zip
 echo "Successfully built app folder"
