831941fa0b585c21cd1b967ce6ae6db175343288
Mitch Webster <Mitchell.Webster@microsoft.com>
2019-09-11 14:13:40 -0700

    try again 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,14 @@ echo "Cloning repository"
 curl "https://github.com/miwebst/ReactSite/archive/a2226e70d6ce57295d336431ab41a6c91b48f00c.zip" -L -o source.zip
 echo "Cloned repository"
 echo $(ls)
-unzip source.zip
+unzip -qq source.zip
+echo "Successfully unzipped repository"
 echo $(ls)
+cd ReactSite-a2226e70d6ce57295d336431ab41a6c91b48f00c
+echo $(ls)
+echo "Building app folder"
+oryx build app -o ../staticsitesoutput/app.zip
+echo "Successfully built app folder"
 
 echo "Hello $1"
 time=$(date)
