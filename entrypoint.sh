cc7b3f2a77e16f08e000a223f6b5d0777e19d72b
Mitch Webster <Mitchell.Webster@microsoft.com>
2019-09-12 15:45:12 -0700

    attempt to copy repo6

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ echo "Application Source Folder: $INPUT_APP_LOCATION"
 echo "Azure Function Source Folder: $INPUT_AZURE_FUNCTION_LOCATION"
 echo "Auzure Pages Build Host: $INPUT_AZURE_PAGES_BUILDHOST"
 UPLOAD_HOST="https://$INPUT_AZURE_PAGES_BUILDHOST/api/values/zip?apiToken=$INPUT_AZURE_PAGES_API_TOKEN"
-echo $UPLOAD_HOST
 
 SHOULD_BUILD_FUNCTION=true
 cd $GITHUB_WORKSPACE
@@ -22,7 +21,13 @@ fi
 # Build App Folder
 echo "Building app folder"
 oryx build app -o /github/staticsitesoutput/app
-echo "Successfully built app folder"
+
+if [ 0 -eq $? ]; then
+	echo "Successfully built app folder"
+else
+	echo "\e[31mFailed to build application\e[0m"
+    exit 1
+fi;
 
 # Zip App Folder
 echo "Zipping app folder"
@@ -36,7 +41,13 @@ if [ SHOULD_BUILD_FUNCTION ]; then
 	cd $GITHUB_WORKSPACE
 	echo "Building api folder"
 	oryx build api -o /github/staticsitesoutput/api
-	echo "Successfully built api folder"
+
+	if [ 0 -eq $? ]; then
+		echo "Successfully built api folder"
+	else
+		echo "\e[31mFailed to build Azure function\e[0m"
+	    exit 1
+	fi;
 
 	echo "Zipping api folder"
 	cd /github/staticsitesoutput/api
@@ -54,4 +65,9 @@ else
 	curl -F "app=@app.zip" -F "event=@$GITHUB_EVENT_PATH" $UPLOAD_HOST
 fi
 
-echo "Successfully uploaded zips"
+if [ 0 -eq $? ]; then
+	echo "Successfully uploaded zips"
+else
+	echo "\e[31mFailed to upload zips\e[0m"
+    exit 1
+fi;
